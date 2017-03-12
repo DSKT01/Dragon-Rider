6d85a2bae1c2e6227635a803de42c189cdf4144c
DSKT01 <diego99gu@hotmail.com>
2017-03-12 14:46:37 -0500

    V0.3

--- Dragon's Rider/Assets/Scripts/Genera.cs
+++ Dragon's Rider/Assets/Scripts/Genera.cs
@@ -1,23 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Genera : MonoBehaviour {
-    public GameObject Plataforma;
-    public Transform PuntoGenerador;
-    public float EspacioEntre;
-    private float Tamano;
-    // Use this for initialization
-    void Start () {
-        Tamano = Plataforma.GetComponent<BoxCollider2D>().size.x;
+public class Genera : MonoBehaviour
+{
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	if(transform.position.x < PuntoGenerador.position.x)
+    public Transform PuntoGenerador;
+    private float EspacioEntre;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        EspacioEntre = 2100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < PuntoGenerador.position.x)
         {
-            transform.position = new Vector3(transform.position.x + Tamano + EspacioEntre, transform.position.y, transform.position.z);
-            Instantiate (Plataforma, transform.position, transform.rotation);
+            transform.position = new Vector3(transform.position.x + EspacioEntre, transform.position.y, transform.position.z);
+
         }
-	}
+    }
 }
