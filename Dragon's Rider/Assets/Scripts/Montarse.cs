ccafe0180fefd34ba51bfdf005b11f08be856bcd
DSKT01 <diego99gu@hotmail.com>
2017-04-02 23:45:08 -0500

    V1.2

---
+++ Dragon's Rider/Assets/Scripts/Montarse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Montarse : MonoBehaviour {
+    public GameObject Roberto;
+    public GameObject Dragon;
+    public Collider2D  RobertoC;
+    public Collider2D  DragonC;
+    public LayerMask PowerUp;
+    private bool Colision;
+    private float segInc = 0;
+    private float segFn = 3;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Colision = Physics2D.IsTouchingLayers(RobertoC, PowerUp);
+        if (Colision)
+        {
+            Roberto.SetActive(false);
+            Dragon.SetActive(true);
+        }
+        while (Dragon.activeInHierarchy)
+        {
+            segInc += Time.deltaTime;
+            if (segInc >= segFn)
+            {
+                Roberto.SetActive(true);
+                Dragon.SetActive(false);
+            }
+        }
+
+    }
+}
--- Dragon's Rider/Assets/Scripts/Movement.cs
+++ Dragon's Rider/Assets/Scripts/Movement.cs
@@ -2,37 +2,54 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-
+    public GameObject Roberto;
+    public GameObject Dragon;
     public float Velocidad;
     public float FSalto;
-    private Rigidbody2D Cuerpo;
-
+    public Rigidbody2D Cuerpo;
     public bool EnSuelo;
     public LayerMask QueEsSuelo;
-    private Collider2D Colision;
+    public Collider2D Colision;
+    public float Speed;
+    public float Jump;
+    public Rigidbody2D Body;
+    public float YMin;
+    public float YMax;
 
 
 
 
-	// Use this for initialization
-	void Start () {
-        Cuerpo = GetComponent<Rigidbody2D>();
-        Colision = GetComponent<Collider2D>();
+    // Use this for initialization
+    void Start () {
+        
 	}
 
     // Update is called once per frame
     void Update() {
 
-        EnSuelo = Physics2D.IsTouchingLayers(Colision, QueEsSuelo);
-
-        Cuerpo.velocity = new Vector2(Velocidad, Cuerpo.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Roberto.activeInHierarchy)
         {
-            if  (EnSuelo)
+            EnSuelo = Physics2D.IsTouchingLayers(Colision, QueEsSuelo);
+
+            Cuerpo.velocity = new Vector2(Velocidad, Cuerpo.velocity.y);
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                Cuerpo.velocity = new Vector2(Cuerpo.velocity.x, FSalto);
+                if (EnSuelo)
+                {
+                    Cuerpo.velocity = new Vector2(Cuerpo.velocity.x, FSalto);
+                }
             }
         }
-        
+        if (Dragon.activeInHierarchy)
+        {
+            Body.velocity = new Vector2(Speed, Body.velocity.y);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Body.velocity = new Vector2(Body.velocity.x, Jump);
+            }
+
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, YMin, YMax), transform.position.z);
+        }
+       
 	}
 }
